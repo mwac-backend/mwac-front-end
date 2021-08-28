35748ed1e29249e5bd6fc2ba3490eb946e0907af
Ooy <suwananpoedpong@gmail.com>
2021-08-28 10:28:44 +0700

    [UPDATE] api heroku

--- src/app/shared/constant/api.constant.ts
+++ src/app/shared/constant/api.constant.ts
@@ -1,6 +1,9 @@
 export class API_URL {
-  static BASE_URL = "http://localhost:3006/api/v1";
+  // static BASE_URL = "http://localhost:3006/api/v1";
 
+  static BASE_URL = "https://mwac.herokuapp.com/api/v1";
+
+  
   static login = API_URL.BASE_URL + '/oauth/login';
   static submission_control = API_URL.BASE_URL + '/submission/control'
   static userInfo = API_URL.BASE_URL + '/user/info'
